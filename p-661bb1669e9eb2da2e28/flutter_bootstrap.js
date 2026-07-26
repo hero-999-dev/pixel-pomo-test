69aff7361bf79156d31395e3308d529d560eedaf
hero-999-dev <282439445+hero-999-dev@users.noreply.github.com>
2026-07-26 12:19:04 +0200

    v34.5: press feedback, typed durations, range hints, polish

--- p-661bb1669e9eb2da2e28/flutter_bootstrap.js
+++ p-661bb1669e9eb2da2e28/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2064259243" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4257121939" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
