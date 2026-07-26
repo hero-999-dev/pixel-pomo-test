2894d960e3efa678f66c9c2efc04613fda669948
hero-999-dev <282439445+hero-999-dev@users.noreply.github.com>
2026-07-26 14:25:10 +0200

    v34.6: stepper realigned, 24 sorted colours, daisy rim, lazy app blocker

--- p-661bb1669e9eb2da2e28/flutter_bootstrap.js
+++ p-661bb1669e9eb2da2e28/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4257121939" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "578212869" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
