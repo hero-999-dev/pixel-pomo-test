cea47f22eff556dd2d47c0f0b3d843976682fa70
hero-999-dev <282439445+hero-999-dev@users.noreply.github.com>
2026-07-27 00:24:03 +0200

    v34.11: undergrowth at the garden edge, tall trees at the back, thinner

--- p-eb0c92ea0d0eb0f76aab/flutter_bootstrap.js
+++ p-eb0c92ea0d0eb0f76aab/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3766127338" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "994033158" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
